65e6efc16701ef3703e91556d7fc6172dad8e1a3
ihklknz <haikalluthfianinobalukia@proton.me>
2023-12-12 02:18:22 +0000

    sdm845-common: cleanup livedisplay

--- sdm845.mk
+++ sdm845.mk
@@ -213,10 +213,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_sdm845
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
